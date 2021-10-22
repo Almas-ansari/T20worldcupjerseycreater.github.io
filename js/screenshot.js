d4a23b2c2cff0b422eabd825873e613752f7f7d9
SANA ERAM <sanaeram5@gmail.com>
2021-10-22 23:48:36 +0530

    resolving issues

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,1 +1,10 @@
-//rollbacked
+document.getElementById('screenshot').addEventListener('click', function() {
+    html2canvas(document.querySelector('#shirtDiv'), {
+        onrendered: function(canvas) {
+            //document.body.appendChild(canvas);
+        return Canvas2Image.saveAsPNG(canvas);
+
+
+        }
+    });
+});
