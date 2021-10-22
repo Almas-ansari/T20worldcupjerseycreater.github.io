a44e832e8a76537e55b38cebc9d27da119cc3d15
SANA ERAM <sanaeram5@gmail.com>
2021-10-22 23:16:16 +0530

    reverting to old code

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,11 +1,1 @@
-
-document.getElementById('screenshot').addEventListener('click', function() {
-        html2canvas(document.querySelector('#shirtDiv'), {
-            onrendered: function(canvas) {
-                //document.body.appendChild(canvas);
-            return Canvas2Image.saveAsPNG(canvas);
-
-
-            }
-        });
-    });
+//rollbacked
