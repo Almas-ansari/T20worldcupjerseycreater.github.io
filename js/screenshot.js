81755ac1d4f417247228670c488161d201ba7328
Almas Ansari <itsmealmas.ansari@gmail.com>
2021-10-23 00:21:07 +0530

    issues fixed

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,11 +1,10 @@
-
 document.getElementById('screenshot').addEventListener('click', function() {
-        html2canvas(document.querySelector('#shirtDiv'), {
-            onrendered: function(canvas) {
-                //document.body.appendChild(canvas);
-            return Canvas2Image.saveAsPNG(canvas);
+    html2canvas(document.querySelector('#shirtDiv'), {
+        onrendered: function(canvas) {
+            //document.body.appendChild(canvas);
+        return Canvas2Image.saveAsPNG(canvas);
 
 
-            }
-        });
+        }
     });
+});
