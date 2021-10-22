8a1ae11b557a10287652e76946f3e833dc29a60a
Almas Ansari <itsmealmas.ansari@gmail.com>
2021-10-22 22:45:10 +0530

    final, one issue left

--- js/screenshot.js
+++ js/screenshot.js
@@ -2,7 +2,7 @@
 document.getElementById('screenshot').addEventListener('click', function() {
         html2canvas(document.querySelector('#shirtDiv'), {
             onrendered: function(canvas) {
-                document.body.appendChild(canvas);
+                //document.body.appendChild(canvas);
             return Canvas2Image.saveAsPNG(canvas);
 
 
